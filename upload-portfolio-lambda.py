fbdc148e7d90a819c590e1d5bc8dd9649d665a87
Jun Fritz <jun.fritz@gmail.com>
2018-06-05 12:50:22 -0700

    add sns functionality on deployment success and failure.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,20 +4,25 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    # TODO implement
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:576461710493:deployPortfolioTopic')
 
-    s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('portfolio.junmarcel.info')
+    try:
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('portfolio.junmarcel.info')
 
-    build_bucket = s3.Bucket('portfoliobuild.junmarcel.info')
-    build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
+        build_bucket = s3.Bucket('portfoliobuild.junmarcel.info')
+        build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    return 'Hello from Lambda'
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully")
+    except:
+        topic.publish(Subject="Failed to Deploy Portfolio", Message="Portfolio deployed unsuccessfully")
