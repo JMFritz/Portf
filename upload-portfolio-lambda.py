04d9d7c7ad9653622e253164293253c5e5c2e1a0
Jun Fritz <jun.fritz@gmail.com>
2018-06-05 13:31:42 -0700

    update lambda script and create deployment pipeline with AWS CodePipeline.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -7,15 +7,26 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:576461710493:deployPortfolioTopic')
 
+    location = {
+        "bucketName": 'portfoliobuild.junmarcel.info',
+        "objectKey": 'portfoliobuild.zip'
+    }
     try:
-        s3 = boto3.resource('s3')
-        portfolio_bucket = s3.Bucket('portfolio.junmarcel.info')
+        job = event.get("CodePipeline.job")
 
-        build_bucket = s3.Bucket('portfoliobuild.junmarcel.info')
-        build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building portfolio from " + str(location)
+        s3 = boto3.resource('s3')
+
+        portfolio_bucket = s3.Bucket('portfolio.junmarcel.info')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -24,5 +35,8 @@ def lambda_handler(event, context):
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject="Failed to Deploy Portfolio", Message="Portfolio deployed unsuccessfully")
