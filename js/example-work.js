b29e839db30073f6fc5bedd9a922f0f2ec208c55
Jun Fritz <jun.fritz@gmail.com>
2018-06-06 13:14:15 -0700

    add structure and sections for summary, projects, certifications, and contact.

---
+++ js/example-work.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ExampleWork extends React.Component {
+  render() {
+    return(
+      <section>It Works!</section>
+    )
+  }
+}
+
+export default ExampleWork;
