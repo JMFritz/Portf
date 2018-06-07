1c168176e01d963e6ec5236a42bef237924c6da5
Jun Fritz <jun.fritz@gmail.com>
2018-06-07 13:00:15 -0700

    add basic react to dynamically place projects and simplify display.

--- js/example-work.js
+++ js/example-work.js
@@ -3,7 +3,31 @@ import React from 'react';
 class ExampleWork extends React.Component {
   render() {
     return(
-      <section>It Works!</section>
+      <section className="sectionTWO section--alignCentered section--description">
+
+        { this.props.work.map( (example, idx) => {
+            return (
+              <ExampleWorkBubble example={example} key={idx}/>
+            )
+          })
+        }
+      </section>
+    )
+  }
+}
+
+class ExampleWorkBubble extends React.Component {
+  render() {
+    let example = this.props.example;
+    return (
+      <div className="projectWrapper">
+        <div className="project">
+          <img alt={ example.image.desc }
+            className="projectImage"
+            src={ example.image.src }/>
+          <h1 className="project-title">{ example.title }</h1>
+        </div>
+      </div>
     )
   }
 }
--- js/main.js
+++ js/main.js
@@ -2,5 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ExampleWork from './example-work';
 
-ReactDOM.render(<ExampleWork />, document.getElementById('example-work'))
- 
+const myWork = [
+  {
+    'title': "Current Project",
+    'image': {
+      'desc': "",
+      'src': "",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Previous Project",
+    'image': {
+      'desc': "",
+      'src': "",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Old Ass Project",
+    'image': {
+      'desc': "",
+      'src': "",
+      'comment': ""
+    }
+  }
+]
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'))
